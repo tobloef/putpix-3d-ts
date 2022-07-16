1602155047f534cfcdb1564d251033fcee0e3636
Tobias Løfgren <tobloef@gmail.com>
2022-07-16 08:12:31 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@ const ctx = canvas.getContext("2d")!;
 
 const rect = ctx.canvas.getBoundingClientRect();
 const dpr = window.devicePixelRatio ?? 1;
-ctx.canvas.width = rect.width * dpr;
-ctx.canvas.height = rect.height * dpr;
+canvas.width = Math.round(rect.width * dpr);
+canvas.height = Math.round(rect.height * dpr);
 ctx.scale(dpr, dpr);
+
+const blankBitmap: number[] = [];
+
+for (let y = 0; y < canvas.height; y++) {
+  for (let x = 0; x < canvas.width; x++) {
+    const i = x * 4 + y * canvas.width * 4;
+    blankBitmap[i] = 0;
+    blankBitmap[i + 1] = 0;
+    blankBitmap[i + 2] = 0;
+    blankBitmap[i + 3] = 255;
+  }
+}
+
+const buffer = new ImageData(Uint8ClampedArray.from(blankBitmap), canvas.width, canvas.height);
 
 function start() {
   update();
@@ -19,24 +33,20 @@ function update() {
 }
 
 function render() {
-  const width = Math.floor(canvas.width);
-  const height = Math.floor(canvas.height);
-
   let t = performance.now() / 1000;
 
-  const bitmap = [];
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const i = x * 4 + y * width * 4;
-      const v = (x + y + Math.round(t * 200)) % 256;
-      bitmap[i] = v >> 1; // Try replacing this with a "v1 / 2"
-      bitmap[i + 1] = 0;
-      bitmap[i + 2] = v;
-      bitmap[i + 3] = 255;
+  for (let y = 0; y < buffer.height; y++) {
+    for (let x = 0; x < buffer.width; x++) {
+      const i = x * 4 + y * buffer.width * 4;
+
+      buffer.data[i] = (i + t * 200)%256;
+      buffer.data[i + 1] = 0;
+      buffer.data[i + 2] = 0;
+      buffer.data[i + 3] = 255;
     }
   }
 
-  ctx.putImageData(new ImageData(Uint8ClampedArray.from(bitmap), width, height), 0, 0);
+  ctx.putImageData(buffer, 0, 0);
 }
 
 start();
